8e8b399c9842dd0f41e8f421dbb34cb29bbe1bbd
Vinay <vinayrajaratnam123@gmail.com>
2018-02-27 16:39:10 -0800

    Added all files

---
+++ VinayShoppingList2.0/VinayShoppingList2.0/MyList.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VinayShoppingList2._0
+{
+    public class MyList
+    {
+        int[] num;
+
+        public MyList()
+        {
+            num = new int[0];
+        }
+
+        public void Add(int value)
+        {
+            int[] temp = new int[num.Length + 1];
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                temp[i] = num[i];
+            }
+
+            temp[temp.Length - 1] = value;
+            num = temp;
+        }
+
+        public bool Remove(int value)
+        {
+            if (Search(value) == true)
+            {
+                int[] temp = new int[num.Length - 1];                
+
+                for (int i = 0, j = 0; i < num.Length; i++, j++)
+                {
+                    if (num[i] != value)
+                    {
+                        temp[j] = num[i];
+                    }
+                    else
+                    {
+                        j--;
+                    }
+                }
+
+                num = temp;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool Search(int value)
+        {
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i] == value)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ VinayShoppingList2.0/VinayShoppingList2.0/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VinayShoppingList2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList list = new MyList();
+            list.Add(1);
+            list.Add(2);
+            list.Add(10);
+            list.Add(13);
+            list.Add(5);
+            list.Add(21);
+
+            list.Remove(13);
+        }
+    }
+}
